98cbfa71669d5ff413b138dde501d229323ff91e
Korrr81 <korkawxneon@gmail.com>
2026-06-29 16:32:11 +0700

    git add week02/

---
+++ week01/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello, my name is supinya siriathonkul\n");
+    printf("Student ID : 692110175\n");
+    printf("DG111 Computer Programming\n");
+    return 0;
+}
